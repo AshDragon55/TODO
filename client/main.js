833bda643fc88ec437392a7397628c7286d563a9
ashDragon55 <ashert1000@gmail.com>
2022-06-30 16:02:03 -0400

    Modalfixed

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import "../lib/collection.js";
-
+import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './main.html';
-import './AddTask.html';
-Template.nav.events({
+import './AddTask/AddTask.html';
+
+Template.addtask.events({
     'click UncompleteTask'(){
         $("#UncompleteTask").UncompleteTask("errorBox");
         listdb.insert({
@@ -13,6 +15,10 @@ Template.nav.events({
 
 
         });
-    }
+    },
+    'click .js-add'() {
+        $("#addModal").modal("show");
+      }
 });
-Template.main.events({});
+
+
